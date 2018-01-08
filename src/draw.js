53867364845b1de65ac5610b786d81ad1558f621
Mike Holm <mholm@rallydev.com>
2018-01-08 12:48:52 -0700

    draw triangles

--- src/draw.js
+++ src/draw.js
@@ -14,7 +14,7 @@ function animate(attributeName, {keyTimes, values, ...rest}) {
       keyTimes: keyTimes.join(";")
     };
   }
-  const str =  svg.animate(Object.assign(opts, init));
+  const str = svg.animate(Object.assign(opts, init));
   return str;
 }
 
@@ -27,39 +27,13 @@ function line(one, two, color) {
   );
 }
 
-function triangle({start, id, one, two, three, dur, color, keyTimes, centroid, brightest, illuminationPoint, extinguishPoint}) {
-  let opts={}, children=[];
-  const path = {
-    d: `M${one.x},${one.y}L${two.x},${two.y}L${three.x},${three.y}z`
-  };
-
-  if(Array.isArray(color)) {
-    if(color.every((c, i, a) => c === a[0])) {
-      opts = {fill: color[0], stroke: color[0]};
-    } else {
-      children = [
-        animate("stroke", {
-          keyTimes,
-          dur,
-          values: color
-        }),
-        animate("fill", {
-          keyTimes,
-          dur,
-          values: color
-        })
-      ];
-    }
-  } else {
-    opts = {fill: color};
-  }
-  const tri = svg.path(Object.assign(opts, path), ...children);
-  // const ill = line(centroid, illuminationPoint, "green");
-  // const ext = line(centroid, extinguishPoint, "cyan");
-  // const proj = line(centroid, brightest.point, "orange");
-  // const dist = line(start, brightest.point, "purple");
-  // return [tri, ill, ext, proj, dist].join("");
-  return tri;
+function triangle({id, one, two, three}) {
+  return svg.path({
+    d: `M${one.x},${one.y}L${two.x},${two.y}L${three.x},${three.y}z`,
+    fill: "black",
+    id,
+    stroke: "white"
+  });
 }
 
 module.exports = {
